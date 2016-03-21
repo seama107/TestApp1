2a148e538a24fd724511969d23403c006e6862e4
Michael <micseam@yahoo.com>
2016-03-21 13:41:14 -0700

    V0.1

---
+++ TestApp1/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  TestApp1
+//
+//  Created by Michael Seaman on 3/21/16.
+//  Copyright © 2016 Bella Seaman. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
